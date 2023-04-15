22808603fd4281384cc4c73afc5bf1b7b83a62e5
Rostislav Dascal <rostislav.dascal@gmail.com>
2023-04-15 14:35:21 +0200

    update title

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 import Link from "next/link";
 
-
 export const metadata = {
-	title: 'Meow! Cat facts for everyone!',
-	description: 'Making the world a better place with daily cat facts! Meow! 😻',
+	title: "Meow!",
+	description: "Making the world a better place with daily cat facts! Meow! 😻",
 };
 
 export default function Home() {
-  return (
-	<>
-		<div className="bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-indigo-800 via-purple-600 to-indigo-800 px-4 py-[12vh]">
-			<h1 className="mx-auto max-w-[800px] text-[5vw] font-black text-white md:text-5xl text-center">
-				Making the world a better place with daily cat facts! <span className="text-yellow-200">Meow! 😻</span>
-			</h1>
-		</div>
+	return (
+		<>
+			<div className="bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-indigo-800 via-purple-600 to-indigo-800 px-4 py-[12vh]">
+				<h1 className="mx-auto max-w-[800px] text-[5vw] font-black text-white md:text-5xl text-center">
+					Making the world a better place with daily cat facts!{" "}
+					<span className="text-yellow-200">Meow! 😻</span>
+				</h1>
+			</div>
 
-		<Link className="mt-20 flex justify-center" href="/facts">
-			<button className="bg-indigo-600 text-white p-6 rounded md:text-2xl text-[4vw]">EXPLORE</button>
-		</Link>
-	</>
-  )
+			<Link className="mt-20 flex justify-center" href="/facts">
+				<button className="bg-indigo-600 text-white p-6 rounded md:text-2xl text-[4vw]">EXPLORE</button>
+			</Link>
+		</>
+	);
 }
