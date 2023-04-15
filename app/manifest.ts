3ad940215de0c3f7377cb654ede3c4eb84efe025
Rostislav Dascal <rostislav.dascal@gmail.com>
2023-04-15 14:13:59 +0200

    update theme color

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,7 +6,7 @@ export default async function Manifest(): Promise<MetadataRoute.Manifest> {
 		short_name: "Meow",
 		description: "A simple app that shows you cat facts",
 		theme_color: "#3730a3",
-		background_color: "#faf5ff",
+		background_color: "#3730a3",
 		icons: [
 			{ src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
 			{ src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
